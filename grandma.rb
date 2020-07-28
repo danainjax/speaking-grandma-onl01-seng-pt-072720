b1b37d97534a1da3ebfd73ef3512e0f9119596de
Dana Pughakoff <danainjax@gmail.com>
2020-07-28 15:32:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase != phrase.downcase
+  elsif phrase = phrase.downcase
      "NO, NOT SINCE 1938!"
-    else phrase == phrase.downcase
+    else phrase != phrase.downcase
     "HUH?! SPEAK UP, SONNY!"
  
   end

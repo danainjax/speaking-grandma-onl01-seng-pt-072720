6287ea6565ab9f3a1160193d172278504fc0b9f8
Dana Pughakoff <danainjax@gmail.com>
2020-07-28 15:27:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase = upcase
+  elsif phrase = phrase.upcase
     puts "NO, NOT SINCE 1938!"
-    elsif phrase == downcase
+    elsif phrase == phrase.downcase
     puts "HUH?! SPEAK UP, SONNY!"
-  else phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+ 
   end
   end
